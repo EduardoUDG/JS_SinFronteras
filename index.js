d076dda03d1a51513c16eefdd79b041bd007cc4a
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-01-13 19:14:20 -0600

    tipos de datos, objetos y arreglo

---
+++ index.js
@@ -0,0 +1,83 @@
+
+/* console.log('Hola mundo'); */
+
+
+// Tipos de datos en JavaScript
+// String
+// Boolean
+// Number
+// Null
+// Undefined
+// Object
+
+
+/* 
+let miPrimeraVariable = 'Esta es mi primera variable';
+console.log(miPrimeraVariable);
+
+miPrimeraVariable = 'Esto a cambiado';
+console.log(miPrimeraVariable);
+*/
+
+/* 
+let texto = 'Este es un texto';
+let numero = 18;
+let booleano = true;
+
+console.log(texto, numero, booleano);
+ */
+
+/* 
+let undef;
+console.log(undef);
+ */
+
+/* 
+let sinDato = null;
+console.log(sinDato);
+*/
+
+
+// objeto vacio
+/* const miPrimerObjeto = {} */
+
+
+
+
+
+// Objeto, agrupacion de datos que hace sentido entre si
+/* const miObjeto = {
+    unNumero: 12,
+    unString: 'Esta es una cadena de caracteres',
+    unaCondicion: true,
+} */
+/* console.log(miObjeto);
+console.log(miObjeto.unNumero);
+ */
+
+// Arreglo vacio
+/* const arrVacio = [] */
+
+
+// Arreglo, los arreglos pueden contener elementos de 
+// cualquier tipo, dentor de el 
+/* 
+const arr = [1, 2, 'Hola', 'Mundo', miObjeto] 
+console.log(arrVacio, arr);
+
+arr.push(5)
+console.log(arrVacio, arr);
+
+let miPrimeraVariable = 'diez'
+arrVacio.push(5)
+arrVacio.push(3)
+arrVacio.push(2)
+arrVacio.push(1)
+arrVacio.push('hola mundo')
+arrVacio.push(miPrimeraVariable)
+console.log(arrVacio);
+*/
+
+
+
+
