35425322065477f031f4a0f6e50503f69ed1b8ce
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-01-18 14:14:55 -0600

    Preparando nuestra aplicacion (form, input, button,ul)

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,4 +1,5 @@
 window.onload = () => {
-    const parrafo = document.getElementById('text')
-    parrafo.innerHTML = '<li>Elemento 1</li><li>Elemento 2</li>'
+    const inputList = document.getElementById('todo');
+
+    console.log(inputList);
 }
