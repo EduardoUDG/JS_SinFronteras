331eaa314d50137107789109355b0652d6b67ad6
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-01-19 16:14:20 -0600

    Simplificando lo anterior con .map y .join

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -9,11 +9,23 @@ window.onload = () => {
         todo.value = '';
         todos.push(todoText);
         
-        
+        /* 
+        .map nos ayuda a 
+        iterar arreglos sin que le tengamos que indicar 
+        desde que posicion iniciar y cuando terminar
+        */
         const todoList = document.getElementById('todo-list');
-        todoList.innerHTML = '';
-        for (let i=0; i < todos.length; i++) {
-            todoList.innerHTML += '<li>' + todos[i] + '</li>';
-        }
+        const todosTemplate =  todos.map(t => '<li>' + t + '</li>');
+        /* 
+        Una forma de traducir la funcion anterior
+        seria. A todos los elementos concatenales la etiqueta <li>
+        al comienzo y al final
+        */
+        todoList.innerHTML = todosTemplate.join('');
+        /* 
+            .join tomar toma todos los elementos del arreglo
+            y los junta mediante lo que nosotros le indiquemos
+            en este caso un string vacio
+        */
     }
 }
