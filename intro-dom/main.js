9222a7913d0e1a916633ed56cc08558de3981dde
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-03-23 12:32:50 -0600

    ejemplo spread operator

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,37 +1,22 @@
-const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-const render = () => {
-    const todoList = document.getElementById('todo-list');
-    const todosTemplate = todos.map(t => '<li>' + t + '</li>');
-    todoList.innerHTML = todosTemplate.join('');
-    const elementos = document.querySelectorAll('#todo-list li');
-    elementos.forEach((elemento, i) => {
-        elemento.addEventListener('click', () => {
-            elemento.parentNode.removeChild(elemento);
-            todos.splice(i, 1)
-            actualizaTodos(todos)
-            render()
-        })
-    })
-}
+const a = { b:1 }
+console.log(a);
 
 
-const actualizaTodos = (todos) => {
-    const todoSctrings = JSON.stringify(todos)
-    localStorage.setItem('todos', todoSctrings)
-}
+const b = a
+console.log(b);
 
 
-window.onload = () => {
-    render()
-    const form = document.getElementById('todo-form');
-    form.onsubmit = (e) => {
-        e.preventDefault();
-        const todo = document.getElementById('todo');
-        const todoText = todo.value;
-        todo.value = '';
-        todos.push(todoText);
-        actualizaTodos(todos)
-        render()
-    }
-}
+b.c = 2
+console.log(b);
+console.log(a);
+console.log(b === a);
+
+//* Esta sintaxis dice, crea un objeto
+//* y todas las propiedades de a
+//* y asignalas a este objeto     
+const c = { ...a }
+c.d = 12
+console.log(c);
+console.log(a);
+console.log(a === c);
