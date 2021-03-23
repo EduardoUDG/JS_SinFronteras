f0bcc4fa87f176d4305abc6600e4b8f5d1586051
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-03-23 14:52:53 -0600

    promesas reject() y catch()

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,5 +1,14 @@
-Promise.resolve(2) 
+// Promise.resolve(2) 
 
-Promise.resolve(2)
+Promise.reject(2)
 .then(valor => valor + 1)
-.then(valor => console.log(valor);)
+.then(valor => console.log(valor))
+.catch(e => console.error(e))
+
+
+const promesa2 = () => {
+    Promise.reject(2)
+    .then(valor => valor + 1)
+    .then(valor => console.log(valor))
+}
+promesa2()
