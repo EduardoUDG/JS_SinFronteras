b0ab5d5a2c70bc86183cc9b2fbaa304c5d4d05f8
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-03-23 14:21:37 -0600

    promesa: resolve() y .then

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,22 +1,5 @@
+Promise.resolve(2) 
 
-const a = { b:1 }
-console.log(a);
-
-
-const b = a
-console.log(b);
-
-
-b.c = 2
-console.log(b);
-console.log(a);
-console.log(b === a);
-
-//* Esta sintaxis dice, crea un objeto
-//* y todas las propiedades de a
-//* y asignalas a este objeto     
-const c = { ...a }
-c.d = 12
-console.log(c);
-console.log(a);
-console.log(a === c);
+Promise.resolve(2)
+.then(valor => valor + 1)
+.then(valor => console.log(valor);)
